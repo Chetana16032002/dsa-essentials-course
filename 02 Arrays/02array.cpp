6b88d8edd6d2180a30093f50f541e9c0e588c620
Chetana16032002 <91176005+Chetana16032002@users.noreply.github.com>
2021-11-11 17:09:42 +0530

    Update 02array.cpp

--- 02 Arrays/02array.cpp
+++ 02 Arrays/02array.cpp
@@ -2,11 +2,15 @@
 #include<iostream>
 using namespace std;
 
-void printArray(int * arr,int n){
+void printArray(int * arr,int n)//*arr is same as passing array int arr[]
+	//here we are passing both arr and n
+	//the size of array in the function is less than main cuz here the array is referenced by address  
+       //passing n too gives the actual result 
+{
 
 
 	cout <<"In Function "<<sizeof(arr) <<endl;
-	arr[0] = 100;
+	arr[0] = 100;//setting this to 100 gives result for function and main same that indicates that both are sharing the same memory
 	for(int i=0;i<n;i++){
 		cout << arr[i] <<endl; 
 	}
